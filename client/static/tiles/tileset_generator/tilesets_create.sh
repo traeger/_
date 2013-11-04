2a94740e5c0a842cdf33af0c949e8bc42edb6648
Marco Traeger <marco.traeger@googlemail.com>
2013-11-04 15:29:15 +0100

    corrected comment

--- client/static/tiles/tileset_generator/tilesets_create.sh
+++ client/static/tiles/tileset_generator/tilesets_create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# create a tilesets-files out of the blender outputs
+# create tilesets-files out of the blender outputs
 
 python tileset.py ../tileset_terrain_grass.png tileset_grass/out_terrain
 python tileset.py ../tileset_terrain_snow.png tileset_snow/out_terrain
